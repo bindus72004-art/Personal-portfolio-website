4c870fb99a3c67258515a9efd12f0564ede8f9c1
bindus72004-art <bindus72004@gmail.com>
2026-01-09 20:13:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Mobile menu
+function toggleMenu(){
+  document.querySelector('.nav-links').classList.toggle('show');
+}
+
+// Scroll animations
+const faders = document.querySelectorAll('.fade');
+
+const observer = new IntersectionObserver(entries=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+  });
+},{threshold:0.2});
+
+faders.forEach(fade=>observer.observe(fade));
